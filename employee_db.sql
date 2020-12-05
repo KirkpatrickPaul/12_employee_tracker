2b5b1151d676da32df60b1197c8667750ac1cacd
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-12-05 17:48:32 -0600

    employee_seeds now works and the DB has several values already seeded in.

--- employee_db.sql
+++ employee_db.sql
@@ -7,7 +7,7 @@ id INT AUTO_INCREMENT PRIMARY KEY
 , first_name VARCHAR(30)
 , last_name VARCHAR(45)
 , role_id INT NOT NULL
-, manager_id INT NOT NULL
+, manager_id INT
 );
 
 CREATE TABLE departments (
---
+++ employee_seeds.sql
@@ -0,0 +1,10 @@
+-- To  make these inserts work, I did this before the ALTER TABLE portion of employee_db.sql
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Morgan", "Freeman", 1, NULL), ("Fred", "Savage", 2, 1), ("Whoopi", "Goldberg", 3, 1), ("Mark", "Wahlberg", 4, 2), ("Tom", "Cruise", 5, 3), ("Winona", "Rider", 5, 5), ("Daniel", "Radcliffe", 5, 5);
+
+INSERT INTO departments (name, manager_id)
+VALUES ("Executive Office", 1), ("Finances", 2), ("Operations", 3), ("Cleaning", 5);
+
+INSERT INTO roles (title, department_id)
+VALUES ("Chief Executive Officer", 1), ("Chief Financial Officer", 1), ("Chief Operations Officer"), ("Accounting Intern", 2), ("Chief Toilet Scrubber", 4), ("Toilet Scrubbing Intern", 4);
