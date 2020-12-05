e71a85dabe27d34c7de6654353ddd0b034409220
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-12-05 17:13:46 -0600

    employee_db.sql now has foreign keys working correctly

--- employee_db.sql
+++ employee_db.sql
@@ -6,17 +6,36 @@ CREATE TABLE employees (
 id INT AUTO_INCREMENT PRIMARY KEY
 , first_name VARCHAR(30)
 , last_name VARCHAR(45)
-, role_id INT NOT NULL REFERENCES roles(id)
+, role_id INT NOT NULL
+, manager_id INT NOT NULL
 );
 
 CREATE TABLE departments (
   id INT AUTO_INCREMENT PRIMARY KEY
   , name VARCHAR(45) NOT NULL
-  , manager_id INT NOT NULL REFERENCES employees(id)
+  , manager_id INT NOT NULL
+  , CONSTRAINT fk_dept_manager_id
+  FOREIGN KEY (manager_id) REFERENCES employees(id) 
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
 );
 
 CREATE TABLE roles (
   id INT AUTO_INCREMENT PRIMARY KEY
   , title VARCHAR(45) NOT NULL
-  , department_id INT NOT NULL REFERENCES departments(id)
+  , department_id INT NOT NULL
+  , CONSTRAINT fk_deptartment_id
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
 );
+
+ALTER TABLE employees ADD CONSTRAINT fk_role_id
+FOREIGN KEY (role_id) REFERENCES roles(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+  
+ALTER TABLE employees ADD CONSTRAINT fk_empl_manager_id
+FOREIGN KEY (manager_id) REFERENCES employees(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
